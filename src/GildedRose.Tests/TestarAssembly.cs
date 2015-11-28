4077919d00c1ca27c316abd474c4d76979b643a6
Raphael Pantaleão <raphaelpanta@gmail.com>
2015-11-28 15:40:19 -0300

    Tradução de arquivos fontes

---
+++ src/GildedRose.Tests/TestarAssembly.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace RosaDourada.Testes
+{
+    public class TestarAssembly
+    {
+        [Fact]
+        public void TestarAVeracidade()
+        {
+            Assert.True(true);
+        }
+    }
+}
